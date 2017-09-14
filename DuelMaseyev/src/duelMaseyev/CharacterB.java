b0ae90d841f410011a5477e5fa08dfb54769bc1c
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-14 15:24:59 -0400

    commit

---
+++ DuelMaseyev/src/duelMaseyev/CharacterB.java
@@ -0,0 +1,14 @@
+package duelMaseyev;
+
+public class CharacterB implements Dueler{
+	
+	public class CharacterB {
+
+	}
+	public void taunt() {
+		System.out.println("Go now");	
+	}
+	public String getName() {
+		return "Daniel Eisen";
+	}
+}
