702b6de1c7013702df3e9d250896fb7c3a5c0e66
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-09-25 14:33:38 -0400

    k

--- DuelMaseyev/src/duelMaseyev/CharacterA.java
+++ DuelMaseyev/src/duelMaseyev/CharacterA.java
@@ -3,31 +3,41 @@ package duelMaseyev;
 public class CharacterA implements Dueler
 {
 	int life;
-public CharacterA() {
+public CharacterA() 
+	{
 
 	}
-	public void taunt() {
+	public void taunt() 
+	{
 		System.out.println("Go now");	
 	}
-	public String getName() {
+	public String getName()
+	{
 		return "Daniel Maseyev";
 	}
 
-	public void setStartingHP(int hp) {
+	public void setStartingHP(int hp) 
+	{
 		this.life = hp;
 	}
-	public int getHP() {
+	public int getHP() 
+	{
 		return this.life;
 	}
-	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-		if(d=hp) {
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) 
+	{
+		if(d.getHP()==hp) 
+		{
 			return true;
 		}
+		return false;
 	}
-	public int getAction(Object caller) {
+	public int getAction(Object caller) 
+	{
 		return caller instanceof a ;
 	}
-	public void hit(Object caller) {
+	public void hit(Object caller) 
+	{
 		this.life = this.life - 20;
 	}
 }
