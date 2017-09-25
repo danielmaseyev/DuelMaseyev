f8a201fd3425b73d44c9136f67f979a6471800d3
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-25 15:30:25 -0400

    asdf

--- DuelMaseyev/src/duelMaseyev/CharacterB.java
+++ DuelMaseyev/src/duelMaseyev/CharacterB.java
@@ -2,13 +2,15 @@ package duelMaseyev;
 
 public class CharacterB implements Dueler{
 	
-	int life;
-	boolean isStartGame = true;
+	private int life;
+	private boolean isStartGame;
+	private boolean isLoaded;
 	public CharacterB() {
-
+		isStartGame=false;
+		isLoaded=false;
 	}
 	public void taunt() {
-		System.out.println("Go now");	
+		System.out.println("Go now!!!!!!");	
 	}
 	public String getName() {
 		return "Daniel Eisen";
@@ -24,7 +26,6 @@ public class CharacterB implements Dueler{
 		return this.life;
 	}
 	
-	//START WORK HERE
 	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
 		if(d.getHP()==hp) {
 			return true;
@@ -32,8 +33,27 @@ public class CharacterB implements Dueler{
 		return false;
 	}
 	public int getAction(Object caller) {
-		return caller instanceof a ;
-	}
+		if(caller instanceof Dueler) {
+			if(isLoaded==false)
+			{
+				if(Math.random()>0.5) {
+					return Duel.GUARDING;
+				}
+				else {
+					isLoaded=true;
+					return Duel.LOADING;
+				}
+			}
+			else {
+				if(Math.random()>0.5) {
+					return Duel.GUARDING
+				}
+				else {
+					return Duel.SHOOTING;
+				}
+			}
+			
+		}
 	public void hit(Object caller) {
 		this.life = this.life-20;
 	}
