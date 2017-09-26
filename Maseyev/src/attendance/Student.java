bc6cddc329bff7add05c021f90afab92e4cd2fad
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-09-26 15:32:18 -0400

    new

--- Maseyev/src/attendance/Student.java
+++ Maseyev/src/attendance/Student.java
@@ -1,5 +1,41 @@
 package attendance;
 
-public class Student {
+public class Student implements Attendee
+{
+	String first;
+	String last;
+	public Student(String firstName, String lastName)
+	{
+		this.first = firstName;
+		this.last = lastName;
+				
+	}
+	public boolean isPresent()
+	{
 
+	}
+	public void setPresent(boolean present)
+	{
+
+	}
+	public String getFirstName()
+	{
+
+	}
+	public String getLastName()
+	{
+
+	}
+	public boolean mathces(String first, String last)
+	{
+
+	}
+	public boolean matches(String last)
+	{
+
+	}
+	public String getReportString()
+	{
+
+	}
 }
