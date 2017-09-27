cb8629baca7977f785400bf0f8e01c7c7618cb9a
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-27 14:55:05 -0400

    asdf

--- DuelMaseyev/src/duelMaseyev/CharacterB.java
+++ DuelMaseyev/src/duelMaseyev/CharacterB.java
@@ -1,74 +1,94 @@
 package duelMaseyev;
-
-public class CharacterB implements Dueler{
-	
-	private int life;
-	private boolean isStartGame;
-	private boolean isLoaded;
-	public CharacterB() {
-		isStartGame=false;
-		isLoaded=false;
-	}
-	public void taunt() {
-		System.out.println("Go now!!!!!!");	
-	}
-	public String getName() {
-		return "Daniel Eisen";
-	}
-
-	public void setStartingHP(int hp) {
-		if(isStartGame==false) {
-			this.life = hp;
-			isStartGame=true;
-		}
-	}
-	public int getHP() {
-		return this.life;
-	}
-	
-	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-		if(d.getHP()==hp) {
-			return true;
-		}
-		else{
-			return false;
-		}
-		
-	}
-	public int getAction(Object caller) {
-		if(caller instanceof Duel) {
-			if(isLoaded==false)
-			{
-				if(Math.random()>0.5) {
-					return Duel.GUARDING;
-				}
-				else {
-					isLoaded=true;
-					return Duel.LOADING;
-				}
-			}
-			else {
-				if(Math.random()>0.5) {
-					return Duel.GUARDING;
-				}
-				else {
-					isLoaded=false;
-					return Duel.SHOOTING;
-				}
-			}
-		}
-		else {
-			return Duel.YEAH_RIGHT;
-			}
-			
-		}
-	public void hit(Object caller) {
-		if(caller instanceof Duel) 
-		{
-			this.life = this.life-20;
-		}
-	}
-
-
-
-}	
+public class CharacterB implements Dueler {
+	 
+    private String name;
+    private String taunt;
+    private int hp;
+    private boolean dloaded;
+ 
+    public CharacterB () {
+ 
+        dloaded = false;
+ 
+    }
+ 
+ 
+    public String getName() {
+ 
+        this.name = "Daniel Eisen";
+        return name;
+    }
+ 
+    public void taunt() {
+ 
+        this.taunt = "Go now!!!";
+        System.out.println(taunt);
+    }
+ 
+    public void setStartingHP(int hp) {
+ 
+        this.hp = hp;
+ 
+        hp = 100;
+    }
+ 
+    public int getHP(){
+ 
+        return hp;
+    }
+ 
+    public boolean determineIfOpponentIsFair(Dueler d, int target) {
+ 
+        if(d.getHP() == target) {
+ 
+            return true;
+        }
+ 
+        return false;
+    }
+ 
+    public int getAction(Object caller) {  
+ 
+        if(caller instanceof Duel ) {
+ 
+            if(dloaded == false){
+ 
+               
+                if(Math.random() < .5) {
+                    dloaded = true;
+                    return Duel.LOADING;
+ 
+                }else {
+                   
+                    return Duel.GUARDING;
+                }
+ 
+            }
+ 
+            else {
+               
+                dloaded = false;
+                return Duel.SHOOTING;
+               
+            }
+        }
+        else {
+ 
+            return Duel.YEAH_RIGHT;
+ 
+        }
+    }
+ 
+    public void hit(Object caller) {
+ 
+ 
+        if(caller instanceof Duel ) {
+ 
+            hp = hp - 10;
+ 
+ 
+        }
+ 
+    }
+ 
+}
